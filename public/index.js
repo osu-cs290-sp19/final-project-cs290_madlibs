9b12da8aed5720982c4a142363370f9f6a6ba46a
hchrist2010 <christhu@oregonstate.edu>
2019-06-05 11:35:22 -0700

    modals working

--- public/index.js
+++ public/index.js
@@ -1,51 +1,111 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+var modal = document.getElementById("modal-backdrop");
+var verbButton = document.getElementById("verb-Button");
+var verbModal = document.getElementById("verb-modal");
+var verbCloseButton = document.getElementById('close-verb-button');
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-} 
+var nounButton = document.getElementById("noun-Button");
+var nounModal = document.getElementById("noun-modal");
+var nounCloseButton = document.getElementById('close-noun-button');
+
+var adjButton = document.getElementById("adj-Button");
+var adjModal = document.getElementById("adj-modal");
+var adjCloseButton = document.getElementById('close-adj-button');
+
+var proButton = document.getElementById("pro-Button");
+var proModal = document.getElementById("pro-modal");
+var proCloseButton = document.getElementById('close-pro-button');
+
+var advButton = document.getElementById("adv-Button");
+var advModal = document.getElementById("adv-modal");
+var advCloseButton = document.getElementById('close-adv-button');
+
+// Open verb
+verbButton.addEventListener('click', function(event){
+  verbModal.classList.toggle('hidden');
+  modal.classList.toggle('hidden');
+});
+
+verbCloseButton.addEventListener('click', function(event){
+  verbModal.classList.toggle('hidden');
+  modal.classList.toggle('hidden');
+});
+
+//noun button
+nounButton.addEventListener('click', function(event){
+  nounModal.classList.toggle('hidden');
+  modal.classList.toggle('hidden');
+});
+
+nounCloseButton.addEventListener('click', function(event){
+  nounModal.classList.toggle('hidden');
+  modal.classList.toggle('hidden');
+});
+
+//adj button
+adjButton.addEventListener('click', function(event){
+  adjModal.classList.toggle('hidden');
+  modal.classList.toggle('hidden');
+});
+
+adjCloseButton.addEventListener('click', function(event){
+  adjModal.classList.toggle('hidden');
+  modal.classList.toggle('hidden');
+});
+
+//pro button
+proButton.addEventListener('click', function(event){
+  proModal.classList.toggle('hidden');
+  modal.classList.toggle('hidden');
+});
+
+proCloseButton.addEventListener('click', function(event){
+  proModal.classList.toggle('hidden');
+  modal.classList.toggle('hidden');
+});
+
+//adv button
+advButton.addEventListener('click', function(event){
+  advModal.classList.toggle('hidden');
+  modal.classList.toggle('hidden');
+});
+
+advCloseButton.addEventListener('click', function(event){
+  advModal.classList.toggle('hidden');
+  modal.classList.toggle('hidden');
+});
+
+
+
 
 // OPENS THE VERB MODAL WHEN THE VERB BUTTON IS CLICKED
-var verbButton = document.getElementById('verb-button');
-var modal = document.getElementById('open-modal');
-var backdrop = document.getElementById('modal-backdrop');
-verbButton.addEventListener('click', function(event) {
-    modal.classList.toggle('hidden');
-    backdrop.classList.toggle('hidden');
-});
-
-// CLOSES THE VERB MODAL WHEN THE CLOSE-VERB BUTTON IS CLICKED
-var closeVerbButton = document.getElementsByClassName('modal-verb-button')[0];
-closeVerbButton.addEventListener('click', function(event){
-    modal.classList.toggle('hidden');
-    backdrop.classList.toggle('hidden');
-});
-
-// OPENS THE NOUN MODAL WHEN THE NOUN BUTTON IS CLICKED
-var nounButton = document.getElementById('noun-button');
-var modal = document.getElementById('open-modal');
-var backdrop = document.getElementById('modal-backdrop');
-nounButton.addEventListener('click', function(event) {
-    modal.classList.toggle('hidden');
-    backdrop.classList.toggle('hidden');
-});
-
-// CLOSES THE NOUN MODAL WHEN THE NOUN BUTTON IS CLICKED
-var closeNounButton = document.getElementsByClassName('modal-noun-button')[0];
-closeNounButton.addEventListener('click', function(event){
-    modal.classList.toggle('hidden');
-    backdrop.classList.toggle('hidden');
-});
-
-
+// var verbButton = document.getElementById('verb-button');
+// var verbModal = document.getElementById('open-modal-verb');
+// var nounModal = document.getElementById('open-modal-noun');
+// var backdrop = document.getElementById('modal-backdrop');
+// verbButton.addEventListener('click', function(event) {
+//     modal.classList.toggle('hidden');
+//     backdrop.classList.toggle('hidden');
+// });
+//
+// // CLOSES THE VERB MODAL WHEN THE CLOSE-VERB BUTTON IS CLICKED
+// var closeVerbButton = document.getElementsByClassName('modal-verb-button')[0];
+// closeVerbButton.addEventListener('click', function(event){
+//     modal.classList.toggle('hidden');
+//     backdrop.classList.toggle('hidden');
+// });
+//
+// // OPENS THE NOUN MODAL WHEN THE NOUN BUTTON IS CLICKED
+// var nounButton = document.getElementById('noun-button');
+// var modal = document.getElementById('open-modal-noun');
+// var backdrop = document.getElementById('modal-backdrop');
+// nounButton.addEventListener('click', function(event) {
+//     modal.classList.toggle('hidden');
+//     backdrop.classList.toggle('hidden');
+// });
+//
+// // CLOSES THE NOUN MODAL WHEN THE NOUN BUTTON IS CLICKED
+// var closeNounButton = document.getElementsByClassName('modal-noun-button')[0];
+// closeNounButton.addEventListener('click', function(event){
+//     modal.classList.toggle('hidden');
+//     backdrop.classList.toggle('hidden');
+// });
