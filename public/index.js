da3e09e486a806ddff3028794e00b3dc668206e4
Aeijan Bajracharya <33380856+bajracae@users.noreply.github.com>
2019-06-05 09:55:36 -0700

    Tried to make the modal work

--- public/index.js
+++ public/index.js
@@ -16,11 +16,44 @@ window.onclick = function(event) {
   }
 } 
 
-var button = document.getElementById('verb-button');
-var modal = document.getElementById('open-verb-modal');
+// function clearModal(label) {
+//     document.getElementById(label). = "";
+// }
+
+// OPENS THE VERB MODAL WHEN THE VERB BUTTON IS CLICKED
+var verbButton = document.getElementById('verb-button');
+var modal = document.getElementById('open-modal');
 var backdrop = document.getElementById('modal-backdrop');
-button.addEventListener('click', function(event) {
+verbButton.addEventListener('click', function(event) {
     modal.classList.toggle('hidden');
     backdrop.classList.toggle('hidden');
     console.log('I am here');
 });
+
+// CLOSES THE VERB MODAL WHEN THE CLOSE-VERB BUTTON IS CLICKED
+var closeVerbButton = document.getElementsByClassName('modal-close-button')[0];
+var verbLabel = document.getElementById('verb-definition');
+closeVerbButton.addEventListener('click', function(event){
+    modal.classList.toggle('hidden');
+    backdrop.classList.toggle('hidden');
+    verbLabel.innerHTML="";
+});
+
+var nounButton = document.getElementById('noun-button');
+var modal = document.getElementById('open-modal');
+var backdrop = document.getElementById('modal-backdrop');
+nounButton.addEventListener('click', function(event) {
+    modal.classList.toggle('hidden');
+    backdrop.classList.toggle('hidden');
+    console.log('I am here');
+});
+
+var closeNounButton = document.getElementsByClassName('modal-close-button')[0];
+var nounLabel = document.getElementById('noun-definition');
+closeNounButton.addEventListener('click', function(event){
+    modal.classList.toggle('hidden');
+    backdrop.classList.toggle('hidden');
+    nounLabel.innerHTML="";
+});
+
+
