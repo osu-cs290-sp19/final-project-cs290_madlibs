2b6aa8406a96c25e98bec57af855bb2e944d7f50
Christopher Perdriau <christopherperdriau99@gmail.com>
2019-06-12 21:03:34 -0700

    I dont know what I changed, Im sorry

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var path = require('path');
 var express = require('express');
-
+var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
+
+app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname,'public')));
 
