6b1fe7ecde1cded12b2aaccda3ff94a5f040dd73
Aeijan Bajracharya <33380856+bajracae@users.noreply.github.com>
2019-06-13 17:40:20 -0700

    Aeijan's files

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
+var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+const dotenv = require('dotenv').config();
+
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.set('view engine', 'handlebars');
+var mongoHost = process.env.MONGO_HOST || '127.0.0.1';
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
+
+// var mongoUrl = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDBName}`;
+var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+
+
+// if(fs.existsSync('.dev')){
+//     mongoUrl = `mongodb://${mongoHost}:${mongoPort}/${mongoDBName}`;
+// }
+
+var db = null;
+
+// app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname,'public')));
 
@@ -23,6 +46,15 @@ app.get('*', function(req, res){
   res.sendFile(path.join(__dirname + '/public/404.html'))
 });
 
-app.listen(port, function () {
-  console.log("== Server is listening on port", port);
+
+MongoClient.connect(mongoURL, { useNewUrlParser: true }, function(err, client) {
+    if(err){
+        throw err;
+    } else {
+        db = client.db(mongoDBName);
+        app.listen(port, function () {
+          console.log("== Server is listening on port", port);
+        });
+    }
 });
+
