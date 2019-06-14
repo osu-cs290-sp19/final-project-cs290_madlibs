eee27350e3f5516b63318734a36430005fa8a5f1
hchrist2010 <christhu@oregonstate.edu>
2019-06-14 03:43:45 -0700

    yes

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
@@ -28,143 +27,87 @@ app.use(bodyParser.json());
 
 app.get('/', function(req, res, next) {
   res.status(200).render('madlibContainer');
+
+  //
+  //
+  // var wordId = db.wordId('noun');
+  // var noun = {
+  //   noun: "Hunter"
+  // };
+  //
+  // wordId.updateOne({$push: {noun: noun}},
+  //   function (err, result) {
+  //          if (err) {
+  //            res.status(500).send({
+  //              error: "Error inserting photo into DB"
+  //            });
+  //          } else {
+  //            console.log("== update result:", result);
+  //            if (result.matchedCount > 0) {
+  //              res.status(200).send("Success");
+  //            } else {
+  //              next();
+  //            }
+  //          }
+  //  }
+ // );
+
+  // var collection = db.collection('people');
+  // var names = {
+  //   name: 'hunter'
+  // };
+
+  // collection.updateOne(
+  //   {$push: { names: name}},
+  //   function (err, result) {
+  //      if (err) {
+  //        res.status(500).send({
+  //          error: "Error inserting photo into DB"
+  //        });
+  //      } else {
+  //        console.log("== update result:", result);
+  //        if (result.matchedCount > 0) {
+  //          res.status(200).send("Success");
+  //        } else {
+  //          next();
+  //        }
+  //      }
+  //    }
+  //);
+
+
 });
 
-app.get('/previous', function(req, res, next){
-  res.status(200).render('previous');
-});
-
-app.get('/previous/:wordId', function(req, res, next) {
-  var wordId = req.params.wordId.toLowerCase();
+app.get('/previous', function(req, res, next) {
   var collection = db.collection('wordType');
-  collection.find({ wordId: wordId}).toArray(function (err, wordType) {
+  collection.find({}).toArray(function (err, wordType) {
 	if (err) {
 	  res.status(500).send({
 		error: "Error fetching people from DB"
 	});
    } else {
 	  console.log("=== wordType: ", wordType);
-	  res.status(200).render('previous', wordType[0]);
-   }
- });
-});
-
-app.post('/previous/:wordId', function(req, res, next) {
-  var wordId = req.params.wordType.toLowerCase();
-  if(req.body && req.body.noun && req.body.verb && req.body.adjective){
-    var collection = db.collection('wordType');
-    collection.updateOne(
-      {wordId: wordId},
-      {$push: {nouns: req.body.noun}},
-      {$push: {verbs: req.body.verb}},
-      {$push: {adjectives: req.body.adjective}},
-      function(err, result){
-        if(err) {
-          res.status(500).send({
-            error: "Error inserting words in DB"
-          });
-        } else {
-          console.log("==update result", result);
-          res.status(200).send("Success");
-        }
-      }
-    );
-  } else {
-    res.status(400).send("Request needs nouns, verbs, and adjectives");
-  }
-});
-
-app.get('*', function(req, res){
-  res.status(404);
-  res.render('404');
-});
-
- MongoClient.connect(mongoURL, { useNewUrlParser: true }, function(err, client) {
-     if(err){
-         throw err;
-     } else {
-         db = client.db(mongoDBName);
-        app.listen(port, function () {
-           console.log("== Server is listening on port", port);
-         });
-     }
-});
-=======
-var fs = require('fs');
-var path = require('path');
-var express = require('express');
-var exphbs = require('express-handlebars');
-var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
-const dotenv = require('dotenv').config();
-
-var app = express();
-var port = process.env.PORT || 3000;
-
- var mongoHost = process.env.MONGO_HOST || '127.0.0.1';
- var mongoPort = process.env.MONGO_PORT || 27017;
- var mongoUser = process.env.MONGO_USER;
- var mongoPassword = process.env.MONGO_PASSWORD;
- var mongoDBName = process.env.MONGO_DB_NAME;
-
-var mongoURL = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDBName}`;
-//var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
-
- var db = null;
-
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.use(express.static('public'));
-app.use(bodyParser.json());
-
-app.get('/', function(req, res, next) {
-  res.status(200).render('madlibContainer');
-});
-
-app.get('/previous/:wordId', function(req, res, next) {
-  var wordId = req.params.wordId.toLowerCase();
-  var collection = db.collection('wordType');
-  collection.find({ wordId: wordId}).toArray(function (err, wordType) {
-	if (err) {
-	  res.status(500).send({
-		error: "Error fetching people from DB"
+	   res.status(200).render('previous', {
+		wordType: wordType
 	});
-   } else {
-	  console.log("=== wordType: ", wordType);
-	  res.status(200).render('previous', wordType[0]);
    }
  });
+   // res.status(200).render("previous");
 });
 
-app.post('/previous/:wordId', function(req, res, next) {
-  var wordId = req.params.wordType.toLowerCase();
-  if(req.body && req.body.noun && req.body.verb && req.body.adjective){
-    var collection = db.collection('wordType');
-    collection.updateOne(
-      {wordId: wordId},
-      {$push: {nouns: req.body.noun}},
-      {$push: {verbs: req.body.verb}},
-      {$push: {adjectives: req.body.adjective}},
-      function(err, result){
-        if(err) {
-          res.status(500).send({
-            error: "Error inserting words in DB"
-          });
-        } else {
-          console.log("==update result", result);
-          res.status(200).send("Success");
-        }
-      }
-    );
-  } else {
-    res.status(400).send("Request needs nouns, verbs, and adjectives");
-  }
-});
+// app.get('save.html', function(req, res, next){
+// 	res.status(200);
+//
 
 app.get('*', function(req, res){
   res.status(404);
   res.render('404');
 });
+
+// app.listen(port, function () {
+//   console.log("== Server is listening on port", port);
+// });
+
 
  MongoClient.connect(mongoURL, { useNewUrlParser: true }, function(err, client) {
      if(err){
@@ -176,4 +119,3 @@ app.get('*', function(req, res){
          });
      }
  });
->>>>>>> ddac74c9788557c493ff6520cc20e139956f753a
