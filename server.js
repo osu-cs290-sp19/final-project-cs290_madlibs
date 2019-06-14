7b82fe11c7aabeaeb78441bbe845036c4afcaa64
Thomas Mahler <mahlert@flip2.engr.oregonstate.edu>
2019-06-13 21:44:37 -0700

    Handlebars working except for sidebar

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/', function(req, res, next){
   // res.status(200);
   // res.sendFile(path.join(__dirname + '/public/index.html'));
   res.status(200);
-  res.render('madLibContainer');
+  res.render('madlibContainer');
 });
 
 app.get('save.html', function(req, res, next){
